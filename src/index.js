b1b1d5ba54857b346f735784c4679823396036a3
Josh Mathiak <jmathiak4@gmail.com>
2022-04-29 19:06:10 -0500

    Added an array that holds relevant info and got hourly weather

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ const APIKey = config.API_KEY;
 
 async function getWeather() {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Dallas%2CTexas/next7days?unitGroup=us&include=days&key=${APIKey}&contentType=json`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Dallas%2CTexas/next7days?unitGroup=us&include=days,hours&key=${APIKey}&contentType=json`,
     { mode: "cors" }
   );
-  const weatherData = await response.json();
-  console.log(weatherData);
+  const weather = await response.json();
+  const days = await queryJSON(weather);
+  console.log(days);
+  console.log(weather);
+}
+
+async function queryJSON(weather) {
+  let interest = [];
+  for (let i = 0; i < weather.days.length; i++) {
+    let day = {
+      low: weather.days[i].tempmin,
+      high: weather.days[i].tempmax,
+      hourly: weather.days[i].hours,
+      condition: weather.days[i].conditions,
+      icon: weather.days[i].icon,
+    };
+    interest.push(day);
+  }
+  return interest;
 }
 
 getWeather();
