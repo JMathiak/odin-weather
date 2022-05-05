060ecb89b491c129f5005531cb4e0c240890cb57
Josh Mathiak <jmathiak4@gmail.com>
2022-05-05 12:43:57 -0500

    Data gets parsed and card content is created

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ const APIKey = config.API_KEY;
 
 async function getWeather() {
   let search = getSearch();
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}/next7days?unitGroup=us&include=days,hours&key=${APIKey}&contentType=json`,
-    { mode: "cors" }
-  );
-  const weather = await response.json();
-  const days = await queryJSON(weather);
-  console.log(weather);
-  return days;
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}/next7days?unitGroup=us&include=days,hours&key=${APIKey}&contentType=json`,
+      { mode: "cors" }
+    );
+    const weather = await response.json();
+    const days = await queryJSON(weather);
+    document.getElementById("locationInput").value = "";
+
+    return days;
+  } catch {
+    alert("No Location Found, Try Again");
+    document.getElementById("locationInput").value = "";
+  }
 }
 
 async function queryJSON(weather) {
   let interest = [];
-  for (let i = 0; i < weather.days.length; i++) {
+  for (let i = 0; i < 5; i++) {
     let day = {
       location: weather.resolvedAddress,
       date: weather.days[i].datetime,
@@ -34,14 +40,44 @@ async function renderDays() {
   let weather = await getWeather();
   console.log(weather);
   let div = document.getElementById("weather");
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
   let locationDiv = document.createElement("div");
-  locationDiv.textContent = weather[0].location;
+  // locationDiv.textContent = weather[0].location;
   div.appendChild(locationDiv);
   weather.forEach((day) => {
     let innerDiv = document.createElement("div");
-    innerDiv.textContent = day.date + " " + day.high;
+    innerDiv.className = "card";
+    let date = parseDate(day.date);
+    let dateDiv = document.createElement("div");
+    dateDiv.className = "date";
+    dateDiv.innerText = date;
+    innerDiv.appendChild(dateDiv);
+    let iconHolder = document.createElement("div");
+    iconHolder.className = "icon";
+    let iconDiv = document.createElement("img");
+    innerDiv.appendChild(iconHolder);
+    iconHolder.appendChild(iconDiv);
+    iconDiv.src = "../icons/" + day.icon + ".svg";
+    let tempDiv = document.createElement("div");
+    tempDiv.className = "temp";
+    tempDiv.textContent =
+      "High: " +
+      Math.round(day.high) +
+      `\u00B0` +
+      " Low: " +
+      Math.round(day.low) +
+      `\u00B0`;
+    innerDiv.appendChild(tempDiv);
+    let condDiv = document.createElement("div");
+    condDiv.innerText = day.condition;
+    condDiv.className = "condition";
+    innerDiv.appendChild(condDiv);
     div.appendChild(innerDiv);
   });
+
+  document.getElementById("container").classList.add(weather[0].icon);
 }
 
 function getSearch() {
@@ -53,6 +89,27 @@ function addEventListener() {
   document
     .getElementById("search-for-weather")
     .addEventListener("click", renderDays);
+}
+
+function parseDate(date) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let splitDate = date.split("-");
+  let month = months[splitDate[1] - 1];
+
+  return month + " " + parseInt(splitDate[2], 10);
 }
 
 addEventListener();
