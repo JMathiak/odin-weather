c332c4256b2834894af8b3f8738a42c15900ba5d
Josh Mathiak <jmathiak4@gmail.com>
2022-05-05 09:55:18 -0500

    Added event listener for search button

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 const APIKey = config.API_KEY;
 
 async function getWeather() {
+  let search = getSearch();
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Dallas%2CTexas/next7days?unitGroup=us&include=days,hours&key=${APIKey}&contentType=json`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}/next7days?unitGroup=us&include=days,hours&key=${APIKey}&contentType=json`,
     { mode: "cors" }
   );
   const weather = await response.json();
   const days = await queryJSON(weather);
+  console.log(weather);
   return days;
 }
 
@@ -15,6 +17,7 @@ async function queryJSON(weather) {
   let interest = [];
   for (let i = 0; i < weather.days.length; i++) {
     let day = {
+      location: weather.resolvedAddress,
       date: weather.days[i].datetime,
       low: weather.days[i].tempmin,
       high: weather.days[i].tempmax,
@@ -31,6 +34,9 @@ async function renderDays() {
   let weather = await getWeather();
   console.log(weather);
   let div = document.getElementById("weather");
+  let locationDiv = document.createElement("div");
+  locationDiv.textContent = weather[0].location;
+  div.appendChild(locationDiv);
   weather.forEach((day) => {
     let innerDiv = document.createElement("div");
     innerDiv.textContent = day.date + " " + day.high;
@@ -38,4 +44,15 @@ async function renderDays() {
   });
 }
 
-//renderDays();
+function getSearch() {
+  let search = document.getElementById("locationInput").value;
+  return search;
+}
+
+function addEventListener() {
+  document
+    .getElementById("search-for-weather")
+    .addEventListener("click", renderDays);
+}
+
+addEventListener();
